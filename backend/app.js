4dbd2fc4324eee575415a1bed0a3c172d98c95ab
mahesh <girimahesh682@gmail.com>
2024-08-10 10:37:20 +0530

    OHK

--- backend/app.js
+++ backend/app.js
@@ -11,19 +11,16 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/api', ticketRoutes);
+// console.log(process.env.MONGO_URI);
 
-
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
+mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log('Connected to MongoDB');
 }).catch(err => {
   console.error('Error connecting to MongoDB', err);
 });
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
